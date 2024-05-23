e4e0f3c851f352712791aeda96c316d2aa0383d5
Khavan Guneratne <khavan@gmail.com>
2024-05-24 06:18:58 +1000

    Remove busy-wait

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,25 +1,19 @@
-let oldHash = "";
-
 function checkHashChanged() {
-    if (window.location.hash != oldHash) {
-        console.log("Hash changed!");
-        oldHash = window.location.hash;
+    let foundElement = null;
 
-        let foundElement = null;
-
-        for (let topic of document.getElementsByClassName("topic")) {
-            if (topic.getAttribute("id") === oldHash.substring(1)) {
-                topic.setAttribute("open", "");
-                foundElement = topic;
-            } else {
-                topic.removeAttribute("open");
-            }
+    for (let topic of document.getElementsByClassName("topic")) {
+        if (topic.getAttribute("id") === window.location.hash.substring(1)) {
+            topic.setAttribute("open", "");
+            foundElement = topic;
+        } else {
+            topic.removeAttribute("open");
         }
+    }
 
-        if (foundElement) {
-            foundElement.scrollIntoView();
-        }
+    // Scroll the topic into view *after* the page has settled.
+    if (foundElement) {
+        foundElement.scrollIntoView();
     }
 }
 
-setInterval(checkHashChanged, 100);
+window.addEventListener("hashchange", checkHashChanged, false);
