823e0e3fa4b76252565baf55e1da817afc562e4b
Alice P. Hacker <awth13@gmail.com>
2024-05-24 07:40:03 +0500

    Put JS in a single script
    The script is short and only used on one page,
    there is no point having multiple files.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,6 @@
+const embedContainer = document.getElementById("player-embed-container");
+const player = document.getElementById("player");
+
 function checkHashChanged() {
     let foundElement = null;
 
@@ -16,5 +19,18 @@ function checkHashChanged() {
     }
 }
 
+function setClip(event, clypId) {
+    embedContainer.innerHTML = '<iframe width="100%" height="265" src="https://clyp.it/' + clypId + '/widget" frameborder="0"></iframe>';
+    player.classList.remove("player-hidden");
+
+    event.preventDefault();
+}
+
+function closePlayer() {
+    embedContainer.innerHTML = "";    
+    player.classList.add("player-hidden");
+}
+
 window.addEventListener("hashchange", checkHashChanged, false);
 window.addEventListener("load", checkHashChanged, false);
+
