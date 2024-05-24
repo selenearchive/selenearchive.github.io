38d89f626636822f3962e7277d0bd60acedc531a
Alice P. Hacker <awth13@gmail.com>
2024-05-24 08:08:42 +0500

    Add click event listeners to clyp.it links

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,7 @@
 const embedContainer = document.getElementById("player-embed-container");
 const player = document.getElementById("player");
 const playerCloseBtn = document.getElementById("player-closebtn")
+const host = "clyp.it"
 
 function checkHashChanged() {
     let foundElement = null;
@@ -20,10 +21,9 @@ function checkHashChanged() {
     }
 }
 
-function setClip(event, clypId) {
-    embedContainer.innerHTML = '<iframe width="100%" height="265" src="https://clyp.it/' + clypId + '/widget" frameborder="0"></iframe>';
+function setClip(event, id) {
+    embedContainer.innerHTML = '<iframe width="100%" height="265" src="https://clyp.it/' + id + '/widget" frameborder="0"></iframe>';
     player.classList.remove("player-hidden");
-
     event.preventDefault();
 }
 
@@ -34,5 +34,12 @@ function closePlayer() {
 
 window.addEventListener("hashchange", checkHashChanged, false);
 window.addEventListener("load", checkHashChanged, false);
-playerCloseBtn.addEventListener("click", closePlayer)
+playerCloseBtn.addEventListener("click", closePlayer);
+
+for (const link of document.getElementsByTagName("a")) {
+    if (link.host === host) {
+        let id = link.pathname.replace('/', '');
+        link.addEventListener("click", (event) => {setClip(event, id)});
+    }
+}
 
